5187e5502ef7cc33f0dc504c288dd75bc3850b43
willi-dev <willi.ilmukomputer@gmail.com>
2017-05-13 20:37:48 +0700

    add post controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,40 +11,32 @@
 angular
     .module('wprestNgApp', [ 'ngAnimate','ngCookies','ngResource','ngRoute','ngSanitize','ngTouch'])
     .config( function( $routeProvider, $locationProvider ) {
+        // set default hashPrefix
+        $locationProvider.hashPrefix('');
+        // route
         $routeProvider
+            // index / home
             .when('/', {
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl',
                 controllerAs: 'main'
             })
             
+            // view about
             .when('/about', {
                 templateUrl: 'views/about.html',
                 controller: 'AboutCtrl',
                 controllerAs: 'about',
             })
 
+            // view single post
             .when('/post/:postid', {
                 templateUrl: 'views/post.html',
                 controller: 'PostCtrl',
                 controllerAs: 'post',
             })
+
             .otherwise({
                 redirectTo: '/'
             });
-        $locationProvider.hashPrefix('');
-    })
-    .controller('TestContent', function( $scope, $http, $routeParams ){
-        console.log( $routeParams.postid );
-        $http.get('http://localhost/_learn/wprest/wp-json/wp/v2/posts/' + $routeParams.postid ).then( function(responseContent ){
-            $scope.post = responseContent;
-            console.log( responseContent );
-        });
     });
-
-    // .controller('MainCtrl', function( $scope, $http, $routeParams){
-    //     $http.get( 'http://localhost/_learn/wprest/wp-json/wp/v2/posts/' ).then( function( response ){
-    //         $scope.posts = response.data;
-    //         console.log( response.data );
-    //     });
-    // });
--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -8,7 +8,7 @@
  * Controller of the wprestNgApp
  */
 angular.module('wprestNgApp')
-  .controller('PostCtrl', function( $scope, $http, $routeParams){
+    .controller('PostCtrl', function( $scope, $http, $routeParams){
         $http.get('http://localhost/_learn/wprest/wp-json/wp/v2/posts/' + $routeParams.postid ).then( function(res ){
             $scope.post = res;
             console.log( res );
